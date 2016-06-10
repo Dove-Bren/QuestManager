1c65348b52f7ea11c0aa165aa91a484c5dab19e0
Matt Broomfield <m0pt0pmatt17@gmail.com>
2016-06-10 09:15:02 -0400

    working working working

--- scripts/maven-release.sh
+++ scripts/maven-release.sh
@@ -2,7 +2,7 @@
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --branch=gh-pages https://${GH_TOKEN}@github.com/Dove-Bren/QuestManager repository
+git clone --branch=repository https://${GH_TOKEN}@github.com/Dove-Bren/QuestManager repository
 
 # Commit and Push the Changes
 cd repository
@@ -11,6 +11,6 @@ DIR='com/skyisland/questmanager/QuestManager/'
 mkdir -p $DIR
 cp -rf ${HOME}/.m2/repository/${DIR}* ${DIR}
 git add -f .
-git commit -m "Lastest maven dependency on successful travis build ${TRAVIS_BUILD_NUMBER} auto-pushed to repository"
+git commit -m "Latest maven dependency on successful travis build ${TRAVIS_BUILD_NUMBER} auto-pushed to repository"
 git push -f origin repository
 
